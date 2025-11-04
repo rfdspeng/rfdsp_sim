53f9826435c5995c3c57a0feee55d797a72487fa
ryanwtsai <tsai.ryanw@gmail.com>
2025-11-04 06:59:07 -0800

    Update CORDIC notes and class def

--- python/rfdsppy/digital_hw_algo.py
+++ python/rfdsppy/digital_hw_algo.py
@@ -9,6 +9,7 @@ and operations
 """
 
 import numpy as np
+from typing import Literal
 
 def round(x: float | int | np.ndarray):
     pass
@@ -52,5 +53,74 @@ def downsample(x: np.ndarray, M: int | float) -> np.ndarray:
     return x[::M].copy()
 
 class CORDIC:
-    def __init__(self, niter):
-        pass
+    """
+    CORDIC
+        
+    """
+
+    def __init__(self, N, mode: Literal["vectoring", "rotation"], sim_type: Literal["vectorized", "hardware"]="vectorized"):
+        """
+        N = number of iterations
+        mode = vectoring or rotation
+
+        """
+
+        self.N = N
+        self.mode = mode
+        self.sim_type = sim_type
+
+        self.K_ = np.prod(1/np.sqrt(1+2**(-2*np.arange(N))))
+        self.theta_i_ = CORDIC.get_rotation_angles(N)
+
+        self.rot_mat_p = [np.array([[1, -1/2**idx],[1/2**idx, 1]]) for idx in range(N)]
+        self.rot_mat_m = [np.array([[1, 1/2**idx],[-1/2**idx, 1]]) for idx in range(N)]
+    
+    @staticmethod
+    def get_rotation_angles(N):
+        """
+        Get the angles of rotation for each iteration (radians)
+
+        """
+        return np.arctan(1/2**(np.arange(N)))
+    
+    def transform(self, I: np.ndarray, Q: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        """
+        In vectoring mode, the input are the rectangular representation I and Q
+        In rotation mode, the inputs are the polar representation env and phase
+        
+        """
+
+        if self.mode == "vectoring":
+            I = I.copy()
+            Q = Q.copy()
+            self.theta_acc_ = np.zeros(I.size)
+
+            self.theta_acc_[I < 0] = np.pi
+            Q[I < 0] = -1*Q[I < 0]
+            Q[I < 0] = -1*I[I < 0]
+
+            env, ph = self.vectoring(I, Q)
+
+        elif self.mode == "rotation":
+            env = I.copy()
+            ph = Q.copy()
+            
+            ph = np.mod(ph, 2*np.pi) # wrap to 0 to 2*pi
+            ph[ph >= np.pi] = ph[ph >= np.pi] - 2*np.pi # wrap to -pi, pi
+            self.final_multiply_ = np.ones(ph.size)
+            self.final_multiply_[(ph < -np.pi/2) and (ph > np.pi/2)] = -1
+            ph[ph < -np.pi/2] = ph[ph < -np.pi/2] + np.pi
+            ph[ph > np.pi/2] = ph[ph > np.pi/2] - np.pi
+            self.err_ = ph
+
+            I, Q = self.rotation(env, ph)
+
+    def vectoring(self, I, Q):
+        if self.sim_type == "vectorized":
+            for i in range(self.N):
+                y_sign = np.sign(Q)
+                
+    
+    def rotation(self, env, ph):
+        if self.sim_type == "vectorized":
+            pass
